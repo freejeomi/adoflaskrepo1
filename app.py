90c015d05eaa483a2ed65d859c8b6eea6d2c2abb
Atuchukwu Ijeoma <freejeomi@yahoo.com>
2025-08-08 03:24:06 +0100

    updating app.py to include connection string

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template, request
-
+import os
+from azure.monitor.opentelemetry import configure_azure_monitor
 app = Flask(__name__)
+
+configure_azure_monitor(
+    connection_string="InstrumentationKey=b842235a-652b-4bd4-8e30-d88023fe552d;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/;ApplicationId=0442abb7-b067-40fb-a7a6-11168242ff88",
+    enable_live_metrics=True
+)
 
 @app.route("/")
 def index():    
