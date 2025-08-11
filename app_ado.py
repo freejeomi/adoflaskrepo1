6ba0fd91bc607c48c6d1143b6d5226011739dc5c
Atuchukwu Ijeoma <freejeomi@yahoo.com>
2025-08-11 19:45:38 +0100

    moving to github

--- app_ado.py
+++ app_ado.py
@@ -4,7 +4,7 @@ from azure.monitor.opentelemetry import configure_azure_monitor
 app = Flask(__name__)
 
 configure_azure_monitor(
-    connection_string = os.getenv("APPINSIGHTS_CONNECTION_STRING"),
+    connection_string="InstrumentationKey=b842235a-652b-4bd4-8e30-d88023fe552d;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/;ApplicationId=0442abb7-b067-40fb-a7a6-11168242ff88",
     enable_live_metrics=True
 )
 
